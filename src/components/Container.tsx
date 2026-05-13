82655f7513ea4e68b58032de1a169feb43f8b9c0
kovacs-mate-tihamer <kovacs.mate.tihamer@students.jedlik.eu>
2026-05-13 10:38:02 +0200

    fajlok/mappak cuccli

--- src/components/Container.tsx
+++
@@ -1,7 +0,0 @@
-type ContainerProps = {
-  children: React.ReactNode;
-};
-
-export default function Container({ children }: ContainerProps) {
-  return <div className="px-6 lg:px-8">{children}</div>;
-}
